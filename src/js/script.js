d130696ba0abc013abcc802bc405d1dae9059bce
CarlosSmariano <sanchesmarianocarlos@gmail.com>
2025-06-26 22:16:42 -0300

    Teste validacao

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,45 @@
-const button = document.querySelector(".button")
-const lidOne = document.querySelector(".lid.one");
-const lidTwo= document.querySelector(".lid.two");
+const apelidoInput = document.getElementById("name");
+const password = document.getElementById("password");
 
+const dados = {
+    nameCorrect: "Pitchuquito",
+    passCorrect: 7032023
+};
 
-button.addEventListener('click', openEnvelope)
+function authenticate() {
+    if (containsBlank()) {
+        if (apelidoInput.value.toLowerCase() == dados.nameCorrect.toLowerCase() && password.value == dados.passCorrect)
+            handleSlideTransition()
 
-function openEnvelope(){
-    lidOne.style.transform = "rotateX(90deg)"
-   setTimeout(() => {
-        lidTwo.style.transform = "rotateX(180deg)";
-    }, 300);
+        else
+            alert("Usuário ou senha incorretos")
+
+    }
+
+}
+
+function containsBlank() {
+    if (apelidoInput.value == '' || password.value == '') {
+        alert("Campos vazios não são permitidos");
+        return false;
+    }
+
+    if (isNaN(password.value)) {
+        alert("A senha é composta apenas por números");
+        return false;
+    }
+
+    return true;
+}
+
+function handleSlideTransition() {
+    const authenticationSection = document.getElementById("authentication");
+    const messageSection = document.getElementById("message");
+
+    authenticationSection.classList.add("hide");
+
+    authenticationSection.addEventListener('transitionend', () => {
+        authenticationSection.style.display = 'none';
+        messageSection.classList.add("show");
+    }, { once: true });
 }
